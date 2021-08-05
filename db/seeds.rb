85b4ca6d5520f8af5c699956d96583b170d8e013
Shogo Miyagi <shogomiyagi94@gmail.com>
2021-08-05 22:36:04 +0900

    Reconfigured the migration files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,42 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+
+puts "Destroying previous data"
+Incident.destroy_all
+User.destroy_all
+
+nomin = User.create!(
+    email: "nmn@gmail.com",
+    password: "111111",
+    first_name: "Nomin",
+    last_name: "Batsaikhan",
+    location: "2-11-3 Meguro, Meguro City, Tokyo",
+    picture: "https://kitt.lewagon.com/placeholder/users/Nomioooob"
+  
+  )
+  
+  aidan = User.create!(
+    email: "aidan@gmail.com",
+    password: "111111",
+    first_name: "Aidan",
+    last_name: "Fournier",
+    location: "1-1 Chiyoda, Chiyoda City, Tokyo",
+    picture: "https://kitt.lewagon.com/placeholder/users/AidanFournier"
+  )
+  
+  sasha = User.create!(
+    email: "sasha@gmail.com",
+    password: "111111",
+    first_name: "Sasha",
+    last_name: "Kaverina",
+    location: "2-1 Chiyoda, Chiyoda City, Tokyo",
+    picture: "https://kitt.lewagon.com/placeholder/users/sashakaverina"
+  )
+  
+  shogo = User.create!(
+    email: "shogo@gmail.com",
+    password: "111111",
+    first_name: "Shogo",
+    last_name: "Miyagi",
+    location: "Tokyo, Shibuya City, Shibuya, 3−21−3",
+    picture: "https://kitt.lewagon.com/placeholder/users/80andAbove"
+  )
