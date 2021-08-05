f44adb19d703db09cdc0f5746f774ba9362229e2
Sasha Kaverina <sasha.kaverina@lewagon.org>
2021-08-05 23:47:45 +0900

    added ssl certificate

--- db/schema.rb
+++ db/schema.rb
@@ -73,9 +73,6 @@ ActiveRecord::Schema.define(version: 2021_08_05_120203) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "nickname"
-    t.string "avatar"
-    t.boolean "victim", default: true
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
