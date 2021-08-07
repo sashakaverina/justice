1288a6da9b4b912d7769cdf1a3ff7f3979479e71
Aidan Fournier <78288118+AidanFournier@users.noreply.github.com>
2021-08-07 19:14:49 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    incidents_path
+    index_path
   end
 
   def after_sign_out_path_for(resource_or_scope)
