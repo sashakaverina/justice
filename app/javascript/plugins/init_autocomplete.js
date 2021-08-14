5189d8aaf394642a11a21e5cf7a96f1af6c403fc
Shogo Miyagi <39066219+80andAbove@users.noreply.github.com>
2021-08-14 14:23:30 +0900

    Update init_autocomplete.js
    fixed init_autocomplete

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -4,9 +4,7 @@ const initAutocomplete = () => {
   const addressInput = document.getElementById('incident_place');
   
   if (addressInput) {
-    places({ container: addressInput }).on('change', function(e) {
-      addressInput.textContent = e.suggestion.value
-    });
+    places({ container: addressInput });
   }
 };
 
