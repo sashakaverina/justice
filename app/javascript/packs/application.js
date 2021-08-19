f57a11936cea9bb5e37d8d88006de75be99c8abf
Shogo Miyagi <shogomiyagi94@gmail.com>
2021-08-19 12:50:00 +0900

    Setup subscriber

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -29,10 +29,12 @@ import "bootstrap";
 import { tagsSearch } from '../components/tags';
 import { initAutocomplete } from '../plugins/init_autocomplete';
 import { buttonPress } from '../components/button_press';
+import { initChatroomCable } from '../channels/chatroom_channel';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   buttonPress();
   initAutocomplete();
   tagsSearch();
+  initChatroomCable();
 });
