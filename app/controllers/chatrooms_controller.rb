66b61e95094558aa55ab4c7b395505ab0893c860
Shogo Miyagi <shogomiyagi94@gmail.com>
2021-08-24 20:22:04 +0900

    Fixed styling in chatroom controller

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -13,17 +13,16 @@ class ChatroomsController < ApplicationController
     end
   end
 
- def update
-  @chatroom = Chatroom.find(params[:id])
-  authorize @chatroom
-  @chatroom.update(chat_params)
- end
+  def update
+    @chatroom = Chatroom.find(params[:id])
+    authorize @chatroom
+    @chatroom.update(chat_params)
+  end
 
-private
+  private
 
- def chat_params
-  # TODO: check your model, might be different than mine
-  params.require(:chatroom).permit(:sender_id, :recipient_id, :status)
- end
-
+  def chat_params
+    # TODO: check your model, might be different than mine
+    params.require(:chatroom).permit(:sender_id, :recipient_id, :status)
+  end
 end
