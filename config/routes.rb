4f6072634bedfb4473daa58e9fa3f38e563c755d
Sasha Kaverina <sasha.kaverina@lewagon.org>
2021-08-26 21:14:47 +0900

    seeds for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   resources :notifications
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :incidents
   resources :collections
 
@@ -13,7 +12,7 @@ Rails.application.routes.draw do
   resources :chatrooms, only: [:show, :update, :index] do
     resources :messages, only: :create
   end
-  get '/index', to: "pages#index"
+
 
   #pdf generating routes
   get '/incidents/:id/report', to: 'incidents#report', as: :report
