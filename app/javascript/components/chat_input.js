154cb57943c75988758166dbd5845f60ea54391a
Shogo Miyagi <shogomiyagi94@gmail.com>
2021-08-27 22:46:12 +0900

    Implemented an approve msg function in JS

--- app/javascript/components/chat_input.js
+++ app/javascript/components/chat_input.js
@@ -1,12 +1,16 @@
 const clearMsg = () => {
     const inputField = () => {
         const chatInput = document.querySelector('.message-content');
+        const approveMsg = document.querySelector('#approve-msg');
         const chatBtn = document.querySelector('.chat-button');
         if(chatInput){
-            if(chatBtn){
-                chatBtn.addEventListener('click', (event) => {
-                    chatInput.innerHTML = ' ';
-                });
+            if(approveMsg){
+                if(chatBtn){
+                    chatBtn.addEventListener('click', (event) => {
+                        chatInput.innerHTML = ' ';
+                        approveMsg.innerHHTML = ' ';
+                    });
+                }
             }
         }
     }
